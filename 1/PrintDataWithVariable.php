7df2e396a6da8fa71d857e6a65ae61f9cde75b2b
lordrians <47350042+lordrians@users.noreply.github.com>
2019-02-05 16:58:18 +0700

    Add files via upload

---
+++ 1/PrintDataWithVariable.php
@@ -0,0 +1,11 @@
+<?php
+    $Nama = "Ferrian Redhia Pratma";
+    $JK = "Laki - laki";
+    $umur = 20;
+
+    
+
+    echo "Nama saya ". $Nama . "<br>";
+    echo "Jenis Kelamin ". $JK ."<br>";
+    echo "Umur saya ". $umur ."<br>";
+?>
